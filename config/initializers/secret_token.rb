37d044849ae58b9873d8025b842d68a8ecee1c74
Vinay <vinay59.grewal@gmail.com>
2013-02-25 01:13:11 +0530

    post scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yoga::Application.config.secret_token = '5d1d473dab5d54170e6bca744614c034632e013d3771d8acd1c6c216deff42b6da7e9a68733a2e6ce4088caaaecc19d96e8973fa7fea606f19cb66176cde828b'
